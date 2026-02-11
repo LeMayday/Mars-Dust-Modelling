721b7222a642fcec616f34ebc64dbd2efc8fe3b5
LeMayday <maychris@gl-login5.arc-ts.umich.edu>
2026-02-11 09:16:52 -0500

    moved heat flux values to mars.py

--- mars.py
+++ mars.py
@@ -6,4 +6,6 @@ p0 = 669            # average surface pressure on Mars
 Ts = -31 + 273      # average surface temperature
 s0 = 580;           # W / m^2
 # Teq = (s0 / 5.67E-8 / 4) ** (1/4)
+q_dot = s0 / 4      # heat flux
+q_dot = q_dot / 2
 
