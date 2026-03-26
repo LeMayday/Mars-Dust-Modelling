ddbc6d7a59dacd2bc224b57dfbd816ecb2eac84d
LeMayday <maychris@gl-login4.arc-ts.umich.edu>
2026-03-26 16:13:58 -0400

    moved dry species to mars.py

--- mars.py
+++ mars.py
@@ -8,6 +8,7 @@ Ts = 200            # K average surface temperature
 gamma = 1.294
 M_bar = 43.45E-3    # kg / mol
 R_gas = 8.31446     # J / mol / K
+species = [{'name': 'dry', 'composition': {'C': 0.95, 'O': 1.90, 'N': 0.06, 'Ar': 0.02}, 'cv_R': 3.4}]
 
 
 # forcing
